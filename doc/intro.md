ea3fb4fc487c12a99a96e93af8d7e8f4370a3c58
Jacob Jenkins <jacob@jacobejenkins.com>
2015-10-16 19:41:35 -0400

    Bunch a cool stuff XD

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to compojure
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/compojure/core_test.clj
@@ -0,0 +1,7 @@
+(ns compojure.core-test
+  (:require [clojure.test :refer :all]
+            [compojure.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
