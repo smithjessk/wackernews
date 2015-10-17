dda2d72a444a80fae7f7cc69b0637a6d6ea5b287
Jess Smith <smith.jessk@gmail.com>
2015-10-16 22:04:13 -0400

    Add top article grabbing

--- src/wackernews/core.clj
+++ src/wackernews/core.clj
@@ -1,5 +1,5 @@
 (ns wackernews.core
-  (:require [wackernews.grabber :refer [grab]]
+  (:require [wackernews.grabber :refer :all]
             [compojure.core :refer :all]
             [org.httpkit.server :refer [run-server]])) ; httpkit is a server
 
@@ -8,5 +8,5 @@
 
 (defn -main []
   (println "Let the wackness begin...")
-  (grab (list "ye" "dog"))
+  (get-top-articles println)
   (run-server wackernews {:port 5000}))
--- src/wackernews/grabber.clj
+++ src/wackernews/grabber.clj
@@ -3,14 +3,25 @@
 
 (def options {:timeout 5000             ; ms
               :basic-auth ["user" "pass"]
+              :query-params {:print "pretty"}
               :user-agent "User-Agent-string"
               :headers {"X-Header" "Value"}})
 
-(def url "https://hacker-news.firebaseio.com/v0/item/8863.json?print=pretty")
+(def url "https://hacker-news.firebaseio.com/v0/item/8863.json")
+
+(def top-articles-url 
+  "https://hacker-news.firebaseio.com/v0/topstories.json")
+
+(defn get-top-articles [callback-fn]
+  (http/get top-articles-url options
+    (fn [{:keys [status headers body error]}]
+      (if error
+        (println "Failed to get latest articles. Exception is " error)
+        (callback-fn body)))))
 
 (defn grab [criteria]
   (http/get url options
     (fn [{:keys [status headers body error]}] 
       (if error
         (println "Failed, exception is " error)
-        (println "Async HTTP GET: " status)))))
+        (println "Async HTTP GET: " body)))))
