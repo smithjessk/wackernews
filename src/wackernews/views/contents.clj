1fb31633f6b01327059da6ceeed14c59c42f6fea
Jacob Jenkins <jacob@jacobejenkins.com>
2015-10-16 22:29:42 -0400

    REAL LINKS

--- src/wackernews/views/contents.clj
+++ src/wackernews/views/contents.clj
@@ -3,6 +3,8 @@
         [hiccup.page :only (html5 include-css include-js)]
         [hiccup.element :only (link-to)]))
 
+(def hn-url "http://news.ycombinator.com/") 
+
 (defn post []
   [:tr 
    [:td 
@@ -13,20 +15,20 @@
         [:span {:class "rank"} "1."]] "      " 
        [:td 
         [:center 
-         [:a {:id "up_10402150", :onclick "return vote(this)", :href "vote?for=10402150&dir=up&auth=764bf5d09d9b274e86125bbeb4790a3f9439ccbc&goto=news"} 
-          [:div {:class "votearrow", :title "upvote"}]]]] 
+         [:a {:id "up_10402150", :onclick "return vote(this)", :href (str hn-url "vote?for=10402150&dir=up&auth=764bf5d09d9b274e86125bbeb4790a3f9439ccbc&goto=news")} 
+          [:div {:class "votearrow", :title "upvote"}]]]]
        [:td {:class "title"} 
         [:span {:class "deadmark"}] 
         [:a {:href "http://rbth.co.uk/multimedia/pictures/2014/04/07/before_the_internet_top_11_soviet_pcs_35711"} "Soviet PCs"] 
         [:span {:class "sitebit comhead"} " (" 
-         [:a {:href "from?site=rbth.co.uk"} 
+         [:a {:href (str hn-url "from?site=rbth.co.uk")} 
           [:span {:class "sitestr"} "rbth.co.uk"]] ")"]]]
       [:tr 
        [:td {:colspan "2"}] 
        [:td {:class "subtext"}  
         [:span {:class "score", :id "score_10402150"} "112 points"] " by " 
-        [:a {:href "user?id=grhmc"} "grhmc"] " " 
-        [:a {:href "item?id=10402150"} "2 hours ago"] "  | " 
-        [:a {:href "flag?id=10402150&auth=764bf5d09d9b274e86125bbeb4790a3f9439ccbc&goto=news"} "flag"] " | " 
-        [:a {:href "item?id=10402150"} "49 comments"] "              "]]  
+        [:a {:href (str hn-url "user?id=grhmc")} "grhmc"] " " 
+        [:a {:href (str hn-url "item?id=10402150")} "2 hours ago"] "  | " 
+        [:a {:href (str hn-url "flag?id=10402150&auth=764bf5d09d9b274e86125bbeb4790a3f9439ccbc&goto=news")} "flag"] " | " 
+        [:a {:href (str hn-url "item?id=10402150")} "49 comments"] "              "]]  
       [:tr {:class "spacer", :style "height:5px"}]]]]])
