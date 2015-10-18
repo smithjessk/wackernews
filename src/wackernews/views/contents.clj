a3b568d4189bbb4665600ff4ac30d019887e7354
Jacob Jenkins <jacob@jacobejenkins.com>
2015-10-17 21:33:11 -0400

    Added sick HN footer 😎

--- src/wackernews/views/contents.clj
+++ src/wackernews/views/contents.clj
@@ -6,28 +6,25 @@
 (def hn-url "http://news.ycombinator.com/")
 
 (defn post [index, story]
-  [:tr 
-   [:td 
-    [:table {:border "0", :cellpadding "0", :cellspacing "0"}
-     [:tbody 
-      [:tr {:class "athing"}
-       [:td {:align "right", :valign "top", :class "title"} 
-        [:span {:class "rank"} (str (+ index 1) ".")]] "      " 
-       [:td 
-        [:center 
-         [:a {:id (str "up_" (get story :id)), :onclick "return vote(this)", :href (str hn-url "vote?for=" (get story :id))} 
-          [:div {:class "votearrow", :title "upvote"}]]]]
-       [:td {:class "title"} 
-        [:span {:class "deadmark"}] 
-        [:a {:href (get story :url)} (get story :title)] 
-        [:span {:class "sitebit comhead"} " (" 
-         [:a {:href (str hn-url "from?site=rbth.co.uk")}
-          [:span {:class "sitestr"} "rbth.co.uk"]] ")"]]]
-      [:tr 
-       [:td {:colspan "2"}] 
-       [:td {:class "subtext"}  
-        [:span {:class "score", :id (str "score_" (get story :id))} (str (get story :score) " points")] " by " 
-        [:a {:href (str hn-url "user?id=" (get story :by))} (get story :by)] " " 
-        [:a {:href (str hn-url "item?id=" (get story :id))} (get story :time)] "  | " 
-        [:a {:href (str hn-url "item?id=" (get story :id))} (str (get story :descendants) " comments")] "              "]]  
-      [:tr {:class "spacer", :style "height:5px"}]]]]])
+  [:span
+   [:tr {:class "athing"}
+    [:td {:align "right", :valign "top", :class "title"} 
+     [:span {:class "rank"} (str (+ index 1) ".")]] "      " 
+    [:td 
+     [:center 
+      [:a {:id (str "up_" (get story :id)), :onclick "return vote(this)", :href (str hn-url "vote?for=" (get story :id))} 
+       [:div {:class "votearrow", :title "upvote"}]]]]
+    [:td {:class "title"} 
+     [:span {:class "deadmark"}] 
+     [:a {:href (get story :url)} (get story :title)] 
+     [:span {:class "sitebit comhead"} " (" 
+      [:a {:href (str hn-url "from?site=rbth.co.uk")}
+       [:span {:class "sitestr"} "rbth.co.uk"]] ")"]]]
+   [:tr 
+    [:td {:colspan "2"}] 
+    [:td {:class "subtext"}  
+     [:span {:class "score", :id (str "score_" (get story :id))} (str (get story :score) " points")] " by " 
+     [:a {:href (str hn-url "user?id=" (get story :by))} (get story :by)] " " 
+     [:a {:href (str hn-url "item?id=" (get story :id))} (get story :time)] "  | " 
+     [:a {:href (str hn-url "item?id=" (get story :id))} (str (get story :descendants) " comments")] "              "]]  
+   [:tr {:class "spacer", :style "height:5px"}]])
--- src/wackernews/views/layout.clj
+++ src/wackernews/views/layout.clj
@@ -15,11 +15,11 @@
                [:table {:border "0", :cellpadding "0", :cellspacing "0", :width "100%", :style "padding:2px"} 
                 [:tbody 
                  [:tr 
-                  [:td {:style "width:18px;padding-right:4px"} 
-                   [:a {:href "http://www.ycombinator.com"} 
-                    [:img {:src "img/logo.png", :width "18", :height "18", :style "border:1px #ffffff solid;"}]]]  
-                  [:td {:style "line-height:12pt; height:10px;"} 
-                   [:span {:class "pagetop"} 
+                  [:td {:style "width:18px;padding-right:4px"}
+                   [:a {:href "http://www.ycombinator.com"}
+                    [:img {:src "img/logo.png", :width "18", :height "18", :style "border:1px #ffffff solid;"}]]]
+                  [:td {:style "line-height:12pt; height:10px;"}
+                   [:span {:class "pagetop"}
                     [:b 
                      [:a {:href "news"} "Wacker News"]] 
                     [:img {:src "img/s.gif", :height "1", :width "10"}] 
@@ -35,4 +35,39 @@
                     [:a {:href "user?id=pg"} "pg"] "\n                (42069) |\n                " 
                     [:a {:href "logout?goto=news"} "logout"]]]]]]]]  
              [:tr {:style "height:10px"}]
-             content]]]]))
+             [:tr 
+              [:td 
+               [:table {:border "0", :cellpadding "0", :cellspacing "0"}  
+                [:tbody 
+                 content
+                 [:tr {:class "spacer", :style "height:5px"}]  
+                 [:tr {:class "morespace", :style "height:10px"}] 
+                 [:tr 
+                  [:td {:colspan "2"}] 
+                  [:td {:class "title"} 
+                   [:a {:href "news?p=2", :rel "nofollow" :style "text-align:left"} "More"]]] ]]]]  
+             [:tr 
+              [:td 
+               [:img {:src "img/s.gif", :height "10", :width "0"}] 
+               [:table {:width "100%", :cellspacing "0", :cellpadding "1"} 
+                [:tbody 
+                 [:tr 
+                  [:td {:bgcolor "#ff6600"}]]]] 
+               [:br] 
+               [:center 
+                [:span {:class "yclinks"} 
+                 [:a {:href "newsguidelines.html"} "Guidelines"] "\n        | " 
+                 [:a {:href "newsfaq.html"} "FAQ"] "\n        | " 
+                 [:a {:href "mailto:hn@ycombinator.com"} "Support"] "\n        | " 
+                 [:a {:href "https://github.com/HackerNews/API"} "API"] "\n        | " 
+                 [:a {:href "security.html"} "Security"] "\n        | " 
+                 [:a {:href "lists"} "Lists"] "\n        | " 
+                 [:a {:href "bookmarklet.html"} "Bookmarklet"] "\n        | " 
+                 [:a {:href "dmca.html"} "DMCA"] "\n        | " 
+                 [:a {:href "http://www.ycombinator.com/apply/"} "Apply to YC"] "\n        | " 
+                 [:a {:href "mailto:hn@ycombinator.com"} "Contact"]] 
+                [:br] 
+                [:br] 
+                [:form {:method "get", :action "//hn.algolia.com/"} "Search:\n          " 
+                 [:input {:type "text", :name "q", :value "" , :size "17"}]]
+                [:br]]]] "      "]]]]))
